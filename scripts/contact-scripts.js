f13a1c7324bf1657aa1f9cb2be60ddf447c3fb61
Jorge <91227064+JRSalica@users.noreply.github.com>
2022-05-17 12:07:04 -0300

    general arrangement

---
+++ scripts/contact-scripts.js
@@ -0,0 +1,17 @@
+function register(event) {
+
+  const el = event.target.elements;
+  event.preventDefault();
+  params = {
+    from_name: 'facurmedina@gmail.com',
+    userName: el.userName.value,
+    user_email: el.user_email.value,
+    tel_number: el.tel_number.value,
+    business: el.business.value,
+    comments: el.comments.value,
+  };
+
+  emailjs.send('service_cocsh87', 'template_jbus0xp', params)
+    .then((resp) => swal ("El mensaje ha sido enviado correctamente.","En breve nos pondremos en contacto para resolver su consulta", "success"))
+    .catch((error) => swal ("Error", "No se pudo enviar el correo, por favor verifique los datos", "error"));
+}
